44d8d206c9a54da3925dfe2622730349d2b0a58f
ah-rasel <18103234@iubat.edu>
2021-12-14 18:46:57 +0600

    prefix and group routes

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.index');
 });
-Route::view('/shops','user.shops')->name('shops');
-Route::view('/shop/shop-name','vendor.single-shop')->name('single.shop');
+Route::view('/shops', 'user.shops')->name('shops');
+Route::view('/shop/shop-name', 'vendor.single-shop')->name('single.shop');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    // Customer
+    Route::prefix('customer')->group(function () {
+        Route::view('/profile','profile')->name('profile');
+    });
+
+    // Vendor
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
