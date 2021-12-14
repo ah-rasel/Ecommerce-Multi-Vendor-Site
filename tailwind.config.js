d2766c931802e857e82ef30765bee1b5c6390067
ah-rasel <18103234@iubat.edu>
2021-12-14 19:46:15 +0600

    Custom Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                lato: ['Lato'],
+                satisfy: ['Satisfy'],
             },
         },
     },
