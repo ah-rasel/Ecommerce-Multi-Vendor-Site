f88a713f38e5ddd96e8c108bcdc6da2402709ed3
ah-rasel <18103234@iubat.edu>
2022-01-02 11:13:45 -0600

    Cart with cart counter

---
+++ app/Http/Livewire/User/ShoppingCart.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Product;
+use Livewire\Component;
+
+class ShoppingCart extends Component
+{
+    protected $listeners = ['AddToCart' => 'AddToCart'];
+    public function AddToCart($id, $quantity)
+    {
+        $product = Product::findOrFail($id);
+        Cart::add($product->id, $product->name, $quantity, $product->price,500);
+        $this->emit('cart-updated');
+    }
+    public function render()
+    {
+        return view('livewire.user.shopping-cart');
+    }
+}
