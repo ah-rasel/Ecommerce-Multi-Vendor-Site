3f2d0383d8fb077db6a4d8f149359cba27d046eb
ah-rasel <18103234@iubat.edu>
2022-01-13 16:10:34 +0600

    Order and Order Products can be created

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -6,8 +6,10 @@ use App\Models\DeliveryAddress;
 use App\Models\DeliveryNumber;
 use App\Models\DeliverySchedule;
 use App\Models\Order;
+use App\Models\OrderProducts;
 use App\Models\User;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Checkout extends Component
@@ -34,18 +36,31 @@ class Checkout extends Component
     }
     public function PlaceOrder()
     {
+        $order=false;
+        DB::transaction(function () {
+            $order = Order::create([
+                'user_id' => auth()->user()->id,
+                'address_id' => $this->address,
+                'number_id' => $this->number,
+                'schedule_id' => $this->schedule,
+                'notes' => $this->notes,
+                'required_date' => now()->addDays(5),
+                'completion_date',
+            ]);
 
-        $order = Order::create([
-            'user_id' => auth()->user()->id,
-            'address_id' => $this->address,
-            'number_id' => $this->number,
-            'schedule_id' => $this->schedule,
-            'notes' => $this->notes,
-            'required_date' => now()->addDays(5),
-            'completion_date',
-        ]);
-        
-        dd($order);
+            $products = Cart::content();
+
+            foreach ($products as $product) {
+                OrderProducts::create([
+                    'order_id' => $order->id,
+                    'product_id' => $product->id,
+                    'quantity' => $product->qty,
+                    'unit_price' => 500,
+                    'discount' => 400,
+                ]);
+            }
+        });
+        return redirect()->route('customer.dashboard');
     }
     public function render()
     {
